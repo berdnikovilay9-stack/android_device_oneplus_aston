946111f04349188b1a045d451ea276d9e18aa712
berdnikovilay9-stack <berdnikovilay9@gmail.com>
2026-08-25 13:26:04 +0300

    Create twrp_aston.mk

---
+++ twrp_aston.mk
@@ -0,0 +1,8 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, device/oneplus/aston/device.mk)
+
+PRODUCT_DEVICE := aston
+PRODUCT_NAME := twrp_aston
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := CPH2609
+PRODUCT_MANUFACTURER := OnePlus
