a2b7a96394e32224c4ba61e91bd59013075a2d5b
berdnikovilay9-stack <berdnikovilay9@gmail.com>
2026-08-25 13:24:43 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_aston.mk
+    $(LOCAL_DIR)/lineage_aston.mk \
+    $(LOCAL_DIR)/twrp_aston.mk
+
+COMMON_LUNCH_MENU_CHOICES := \
+    lineage_aston-userdebug \
+    twrp_aston-eng
